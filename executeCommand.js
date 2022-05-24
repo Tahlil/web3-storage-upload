c93c2b067078e36fa007090d31b4cf5ae744f4c0
tahlil <bsse0803@iit.du.ac.bd>
2022-05-24 11:55:32 +0600

    Read file names from files folder

--- executeCommand.js
+++ executeCommand.js
@@ -1,13 +1,29 @@
 const { exec } = require("child_process");
-
-exec("ls -la", (error, stdout, stderr) => {
-    if (error) {
-        console.log(`error: ${error.message}`);
-        return;
-    }
-    if (stderr) {
-        console.log(`stderr: ${stderr}`);
-        return;
-    }
-    console.log(`stdout: ${stdout}`);
+//requiring path and fs modules
+const path = require('path');
+const fs = require('fs');
+//joining path of directory 
+const directoryPath = path.join(__dirname, 'files');
+//passsing directoryPath and callback function
+fs.readdir(directoryPath, function (err, files) {
+    //handling error
+    if (err) {
+        return console.log('Unable to scan directory: ' + err);
+    } 
+    console.log("File names:");
+    //listing all files using forEach
+    files.forEach(function (file) {
+        console.log(file); 
+    });
 });
+// exec("ls -la", (error, stdout, stderr) => {
+//     if (error) {
+//         console.log(`error: ${error.message}`);
+//         return;
+//     }
+//     if (stderr) {
+//         console.log(`stderr: ${stderr}`);
+//         return;
+//     }
+//     console.log(`stdout: ${stdout}`);
+// });
